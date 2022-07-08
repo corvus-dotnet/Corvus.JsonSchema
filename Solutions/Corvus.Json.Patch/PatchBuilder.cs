41b5f3f2c75e287cbd16bb65108fffcede654df6
Matthew Adams <matthew.adams@endjin.com>
2022-07-08 09:46:39 +0100

    Renamed file to match type name.

--- Solutions/Corvus.Json.Patch/PatchBuilder.cs
+++ Solutions/Corvus.Json.Patch/PatchBuilder.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="Patch.cs" company="Endjin Limited">
+﻿// <copyright file="PatchBuilder.cs" company="Endjin Limited">
 // Copyright (c) Endjin Limited. All rights reserved.
 // </copyright>
 
