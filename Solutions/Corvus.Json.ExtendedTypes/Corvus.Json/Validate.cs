2ad415f91acf65332053c9d6f1e05b9a9ea4de2e
Matthew Adams <matthew.adams@endjin.com>
2022-09-09 10:56:05 +0100

    Low-allocation relative JSON pointer validation.

--- Solutions/Corvus.Json.ExtendedTypes/Corvus.Json/Validate.cs
+++ Solutions/Corvus.Json.ExtendedTypes/Corvus.Json/Validate.cs
@@ -804,7 +804,7 @@ public static partial class Validate
 
             if (context.Level == ValidationLevel.Verbose)
             {
-                result = context.Context.WithResult(isValid: true, "Validation 6.1.1 type - was a 'string' with format 'duration'.");
+                result = context.Context.WithResult(isValid: true, "Validation 6.1.1 type - was a 'string' with format 'email'.");
             }
 
             return true;
@@ -839,30 +839,54 @@ public static partial class Validate
             }
         }
 
-        JsonString jsonPointer = instance.AsString;
-        if (!RelativeJsonPointerPattern.IsMatch(jsonPointer))
+        ValidationContext result = validationContext;
+
+        if (instance.HasJsonElementBacking)
         {
-            if (level >= ValidationLevel.Detailed)
-            {
-                return validationContext.WithResult(isValid: false, $"Validation 6.1.1 type - should have been 'string' with format 'relative-json-pointer' but was '{jsonPointer}'.");
-            }
-            else if (level >= ValidationLevel.Basic)
-            {
-                return validationContext.WithResult(isValid: false, "Validation 6.1.1 type - should have been a 'string' with format 'relative-json-pointer'.");
-            }
-            else
-            {
-                return validationContext.WithResult(isValid: false);
-            }
+            // We know it is a string, so we should always return true, no need to check the result.
+            instance.AsJsonElement.TryGetValue(RelativePointerValidator, new ValidationContextWrapper(result, level), out result);
+        }
+        else
+        {
+            RelativePointerValidator(instance.AsString.AsSpan(), new ValidationContextWrapper(result, level), out result);
         }
 
-        if (level == ValidationLevel.Verbose)
+        if (level == ValidationLevel.Flag && !result.IsValid)
         {
-            return validationContext
-                .WithResult(isValid: true, "Validation 6.1.1 type - was a 'string' with format 'relative-json-pointer'.");
+            return result;
         }
 
-        return validationContext;
+        return result;
+
+        static bool RelativePointerValidator(ReadOnlySpan<char> input, in ValidationContextWrapper context, out ValidationContext result)
+        {
+            result = context.Context;
+
+            if (!RelativeJsonPointerPattern.IsMatch(input))
+            {
+                if (context.Level >= ValidationLevel.Detailed)
+                {
+                    result = context.Context.WithResult(isValid: false, $"Validation 6.1.1 type - should have been 'string' with format 'relative-json-pointer', but was '{input}'.");
+                }
+                else if (context.Level >= ValidationLevel.Basic)
+                {
+                    result = context.Context.WithResult(isValid: false, "Validation 6.1.1 type - should have been a 'string' with format 'relative-json-pointer'.");
+                }
+                else
+                {
+                    result = context.Context.WithResult(isValid: false);
+                }
+
+                return false;
+            }
+
+            if (context.Level == ValidationLevel.Verbose)
+            {
+                result = context.Context.WithResult(isValid: true, "Validation 6.1.1 type - was a 'string' with format 'relative-json-pointer'.");
+            }
+
+            return true;
+        }
     }
 
     /// <summary>
