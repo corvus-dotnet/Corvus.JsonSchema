084c0988816b252c305c9f00508dd4a7ab9c8713
Matthew Adams <matthew.adams@endjin.com>
2022-09-09 11:58:39 +0100

    Switch to using static instance of IdnMapping for IdnEmail validation.

--- Solutions/Corvus.Json.ExtendedTypes/Corvus.Json/Validate.cs
+++ Solutions/Corvus.Json.ExtendedTypes/Corvus.Json/Validate.cs
@@ -349,11 +349,8 @@ public static partial class Validate
             // Examines the domain part of the email and normalizes it.
             static string DomainMapper(Match match)
             {
-                // Use IdnMapping class to convert Unicode domain names.
-                var idn = new IdnMapping();
-
                 // Pull out and process domain name (throws ArgumentException on invalid)
-                string domainName = idn.GetAscii(match.Groups[2].Value);
+                string domainName = IdnMapping.GetAscii(match.Groups[2].Value);
 
                 return match.Groups[1].Value + domainName;
             }
