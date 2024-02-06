adcee7c91ebcb3ab17b00e26ee02ec42fe4a26be
James Dawson <james.dawson@endjin.com>
2024-02-06 10:29:19 +0000

    Bump InvokeBuild and build module versions

--- build.ps1
+++ build.ps1
@@ -71,10 +71,10 @@ param (
     [string] $BuildModulePath,
 
     [Parameter()]
-    [version] $BuildModuleVersion = "1.3.8",
+    [version] $BuildModuleVersion = "1.5.4",
 
     [Parameter()]
-    [version] $InvokeBuildModuleVersion = "5.7.1"
+    [version] $InvokeBuildModuleVersion = "5.10.1"
 )
 
 $ErrorActionPreference = $ErrorActionPreference ? $ErrorActionPreference : 'Stop'
