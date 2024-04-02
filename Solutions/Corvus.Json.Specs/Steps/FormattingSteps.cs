6c91a5fc0751693c876b50f02c61232421c355e3
Matthew Adams <matthew.adams@endjin.com>
2024-04-02 16:39:14 +0100

    `Update Formatting.cs and add new feature in Formatting.feature` (#327)
    ` `
    
    `In the `Formatting.cs` file, added a line of code to increment the `setIndex` variable within a conditional statement that checks if the count of uppercase characters is more than 2. This prevents it from overwriting the character in the output in this case. This is effectively a breaking change, but brings the behaviour in line with that expected.
    
    Modified the behavior when a digit character is encountered so that it does *not* capitalize the next character. This is a potentially breaking change for types with numbers in them that have strange capitalization.
    
    In the `Formatting.feature` file, introduced a new feature for formatting identifiers with a scenario outline for formatting strings.
    
    Added a new class `FormattingSteps` in the `FormattingSteps.cs` file that contains step definitions for the formatting scenarios. This includes methods for formatting the input string with Pascal casing and checking that the output matches the expected result.

---
+++ Solutions/Corvus.Json.Specs/Steps/FormattingSteps.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="FormattingSteps.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+using Corvus.Json.CodeGeneration;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Steps;
+
+/// <summary>
+/// Steps for the formatting scenarios.
+/// </summary>
+[Binding]
+public class FormattingSteps
+{
+    private string formatted = string.Empty;
+
+    [When(@"I format the input string ""([^""]*)"" with Pascal casing")]
+    public void WhenIFormatTheInputStringWithPascalCasing(string input)
+    {
+        this.formatted = Formatting.ToPascalCaseWithReservedWords(input).ToString();
+    }
+
+    [Then(@"The output will be ""([^""]*)""")]
+    public void ThenTheOutputWillBe(string expected)
+    {
+        Assert.AreEqual(expected, this.formatted);
+    }
+}
