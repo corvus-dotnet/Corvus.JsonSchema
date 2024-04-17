3f2336fc392e349e619d7838d7e080338708be32
James Dawson <james.dawson@endjin.com>
2024-04-17 20:23:30 +0100

    Remove prior test framework version handling

--- build.ps1
+++ build.ps1
@@ -182,13 +182,6 @@ task PreTest {
         $script:LogLevelBackup = $LogLevel
         $script:LogLevel = "quiet"
     }
-
-    # if ($IsLinux) {
-        $script:AdditionalTestArgs += @("--framework", "net8.0")
-    # }
-
-    Write-Host "DEBUG: Show compiled specs assemblies:"
-    gci Solutions/Corvus.Json.Specs/bin/$Configuration -Filter *.Specs.dll -Recurse | Out-String | Write-Host
 }
 task PostTest {
     # Revert back to original logging level
