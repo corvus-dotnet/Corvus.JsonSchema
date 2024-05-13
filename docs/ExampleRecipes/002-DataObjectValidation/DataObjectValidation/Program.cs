cb10d1c44c6c51f443c2d9ed2d0d2234a1a993be
Matthew Adams <matthew.adams@endjin.com>
2024-05-13 14:14:22 +0100

    Adding the examples from the recipe blog posts.

---
+++ docs/ExampleRecipes/002-DataObjectValidation/DataObjectValidation/Program.cs
@@ -0,0 +1,26 @@
+﻿using Corvus.Json;
+using JsonSchemaSample.Api;
+using NodaTime;
+
+// Required fields are not-nullable in the create method
+var personConstraints = PersonConstraints.Create(
+    birthDate: new LocalDate(1820, 1, 17),
+    familyName: "Brontë",
+    givenName: "Anne",
+    height: 1.52,
+    // Invalid string length
+    otherNames: string.Empty);
+
+// Fast, zero-allocation boolean-only validation
+if (!personConstraints.IsValid())
+{
+    // Detailed validation (best used only when fast validation indicates an invalid condition)
+    var validationResults = personConstraints.Validate(ValidationContext.ValidContext, ValidationLevel.Detailed);
+    foreach (var result in validationResults.Results)
+    {
+        Console.WriteLine(result);
+    }
+}
+
+// Conversion to double is available implicitly as it is now constrained by format, and does not allocate.
+double heightValue = personConstraints.Height;
---
+++ docs/ExampleRecipes/010-CreatingTuples/CreatingTuples/Program.cs
@@ -0,0 +1,36 @@
+﻿using Corvus.Json;
+using JsonSchemaSample.Api;
+
+// Create a tuple from a dotnet tuple
+(int, string, bool) dotnetTuple = (3, "Hello", false);
+ThreeTuple threeTuple = dotnetTuple;
+
+// Create a tuple directly
+ThreeTuple threeTuple2 = ThreeTuple.Create(3, "Hello", false);
+
+// Parse a JSON array
+string threeTupleJson =
+    """
+    [3, "Hello", false]
+    """;
+
+using ParsedValue<ThreeTuple> threeTuple3 = ParsedValue<ThreeTuple>.Parse(threeTupleJson);
+
+if (threeTuple3.Instance == threeTuple)
+{
+    // The tuples are equal
+    Console.WriteLine("The tuples are equal");
+}
+else
+{
+    Console.WriteLine("The tuples are not equal");
+}
+
+// Access the item values
+Console.WriteLine($"{threeTuple.Item1}, {threeTuple.Item2}, {threeTuple.Item3}");
+
+// Convert to a dotnet tuple
+(JsonInt32, JsonString, JsonBoolean) tupleFromThreeTuple = threeTuple;
+
+// Automatically convert to a tuple where implicit conversions are available
+(int, JsonString, bool) dotnetTupleFromThreeTuple = threeTuple;
