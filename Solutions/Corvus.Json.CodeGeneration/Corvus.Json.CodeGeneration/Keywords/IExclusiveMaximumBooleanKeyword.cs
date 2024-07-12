3c11a0daa941996b36a606797c030cec68f1d808
Matthew Adams <matthew.adams@endjin.com>
2024-07-12 12:58:14 +0100

    Added engine for OpenApi30
    - Added additional keywords for OpenApi30 dialect.

---
+++ Solutions/Corvus.Json.CodeGeneration/Corvus.Json.CodeGeneration/Keywords/IExclusiveMaximumBooleanKeyword.cs
@@ -0,0 +1,10 @@
+﻿// <copyright file="IExclusiveMaximumBooleanKeyword.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Corvus.Json.CodeGeneration;
+
+/// <summary>
+/// A keyword that represents the boolean exclusivity modifier for a maximum keyword.
+/// </summary>
+public interface IExclusiveMaximumBooleanKeyword : IKeyword;
