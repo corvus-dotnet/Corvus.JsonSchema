5f1b78583675fee889104cac97b1f716e4540ea6
Matthew Adams <matthew.adams@endjin.com>
2024-07-15 18:52:55 +0100

    Updated to use the new UriTemplates code.

---
+++ Solutions/Corvus.Json.CodeGeneration/Corvus.Json.CodeGeneration/Keywords/IStringRegexValidationProviderKeyword.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="IStringRegexValidationProviderKeyword.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Corvus.Json.CodeGeneration;
+
+/// <summary>
+/// A keyword that provides a single validation regular expression for a string.
+/// </summary>
+/// <remarks>
+/// These are regular expression values that may be used by code generators
+/// to minimize overhead when implementing validators. They may be
+/// cached, or provided as static values as appropriate.
+/// </remarks>
+public interface IStringRegexValidationProviderKeyword : IStringValidationKeyword, IValidationRegexProviderKeyword
+{
+}
