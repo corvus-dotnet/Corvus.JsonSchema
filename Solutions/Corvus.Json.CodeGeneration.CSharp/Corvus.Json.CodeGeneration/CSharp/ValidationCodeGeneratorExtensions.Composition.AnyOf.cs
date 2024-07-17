be298d7f8c87b4492443177ab7acbbf8f1003d7c
Matthew Adams <matthew.adams@endjin.com>
2024-07-17 09:49:43 +0100

    Added any-of subschema validation.

---
+++ Solutions/Corvus.Json.CodeGeneration.CSharp/Corvus.Json.CodeGeneration/CSharp/ValidationCodeGeneratorExtensions.Composition.AnyOf.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="ValidationCodeGeneratorExtensions.Composition.AnyOf.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+#if !NET8_0_OR_GREATER
+using System.Buffers;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+#endif
+
+namespace Corvus.Json.CodeGeneration.CSharp;
+
+/// <summary>
+/// Extensions to <see cref="CodeGenerator"/> for validation.
+/// </summary>
+public static partial class ValidationCodeGeneratorExtensions
+{
+    /// <summary>
+    /// Append a validation method for all-of composite types.
+    /// </summary>
+    /// <param name="generator">The code generator.</param>
+    /// <param name="methodName">The name of the validation method.</param>
+    /// <param name="typeDeclaration">The type declaration which requires string validation.</param>
+    /// <param name="children">The child handlers for the <see cref="IKeywordValidationHandler"/>.</param>
+    /// <returns>A reference to the generator having completed the operation.</returns>
+    /// <param name="parentHandlerPriority">The parent validation handler priority.</param>
+    public static CodeGenerator AppendCompositionAnyOfValidation(
+        this CodeGenerator generator,
+        string methodName,
+        TypeDeclaration typeDeclaration,
+        IReadOnlyCollection<IChildValidationHandler> children,
+        uint parentHandlerPriority)
+    {
+        return generator
+            .BeginReservedMethodDeclaration(
+                "public static",
+                "ValidationContext",
+                methodName,
+                new("in", typeDeclaration.DotnetTypeName(), "value"),
+                ("in ValidationContext", "validationContext"),
+                ("ValidationLevel", "level", "ValidationLevel.Flag"))
+                .ReserveName("result")
+                .ReserveName("isValid")
+                .AppendBlockIndent(
+                """
+                ValidationContext result = validationContext;
+                ValidationContext childContextBase = result;
+                """)
+            .AppendCompositionAnyOfValidation(typeDeclaration, children)
+            .AppendSeparatorLine()
+            .AppendLineIndent("return result;")
+            .EndMethodDeclaration();
+    }
+
+    private static CodeGenerator AppendCompositionAnyOfValidation(
+        this CodeGenerator generator,
+        TypeDeclaration typeDeclaration,
+        IReadOnlyCollection<IChildValidationHandler> children)
+    {
+        foreach (IChildValidationHandler child in children)
+        {
+            child.AppendValidationCode(generator, typeDeclaration);
+        }
+
+        return generator;
+    }
+}
