905798156a00daeda1224f07e308f15e62062f65
Matthew Adams <matthew.adams@endjin.com>
2024-07-29 15:00:44 +0100

    Added built-in name heuristics for the basic types (not format-based) Fixed the array type name heuristic so it works with single implied items types.

---
+++ Solutions/Corvus.Json.CodeGeneration.CSharp/Corvus.Json.CodeGeneration/CSharp/NameHeuristics/BuiltInNullTypeNameHeuristic.cs
@@ -0,0 +1,60 @@
+﻿// <copyright file="BuiltInNullTypeNameHeuristic.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Corvus.Json.CodeGeneration.CSharp;
+
+/// <summary>
+/// A name heuristic based on a built-in boolean type.
+/// </summary>
+public sealed class BuiltInNullTypeNameHeuristic : IBuiltInTypeNameHeuristic
+{
+    private BuiltInNullTypeNameHeuristic()
+    {
+    }
+
+    /// <summary>
+    /// Gets a singleton instance of the <see cref="BuiltInNullTypeNameHeuristic"/>.
+    /// </summary>
+    public static BuiltInNullTypeNameHeuristic Instance { get; } = new();
+
+    /// <inheritdoc/>
+    public bool IsOptional => false;
+
+    /// <inheritdoc/>
+    public uint Priority => 1;
+
+    /// <inheritdoc/>
+    public bool TryGetName(TypeDeclaration typeDeclaration, JsonReferenceBuilder reference, Span<char> typeNameBuffer, out int written)
+    {
+        written = 0;
+
+        if ((typeDeclaration.AllowedCoreTypes() & CoreTypes.Null) != 0 &&
+            typeDeclaration.AllowedCoreTypes().CountTypes() == 1)
+        {
+            // We are a simple string type
+            foreach (IKeyword keyword in typeDeclaration.Keywords())
+            {
+                if (keyword is ICoreTypeValidationKeyword)
+                {
+                    continue;
+                }
+
+                if (keyword is INonStructuralKeyword)
+                {
+                    // Don't worry if it is a non-structural keyword.
+                    continue;
+                }
+
+                // This is "some other" structural keyword, so we can't continue.
+                return false;
+            }
+
+            typeDeclaration.SetDotnetNamespace("Corvus.Json");
+            typeDeclaration.SetDotnetTypeName("JsonNull");
+            return true;
+        }
+
+        return false;
+    }
+}
