e0cac5a9995dc5927d5c49c1711989ecbf8c9721
Matthew Adams <matthew.adams@endjin.com>
2024-08-02 14:21:37 +0100

    Added an example of the Match() code.

--- Solutions/Sandbox/Program.cs
+++ Solutions/Sandbox/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using Corvus.Json;
 using JsonSchemaSample.Api;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using NodaTime;
 
 Person audreyJones =
@@ -10,3 +11,10 @@ Person audreyJones =
                 otherNames: PersonNameElementArray.FromItems("Margaret", "Nancy"),
                 familyName: "Jones"),
         dateOfBirth: new LocalDate(1947, 11, 7));
+
+string result = audreyJones.Name.OtherNames.Match(
+    static (in PersonNameElement otherNames) => $"Other names: {otherNames}",
+    static (in PersonNameElementArray otherNames) => $"Other names: {string.Join(", ", otherNames)}",
+    static (in OtherNames value) => throw new InvalidOperationException($"Unexpected type: {value}"));
+
+Console.WriteLine(result);
