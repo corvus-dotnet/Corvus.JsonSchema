09c06c80af5404a23db62baff80428c9648c1f5b
Matthew Adams <matthew.adams@endjin.com>
2024-08-08 02:22:24 +0100

    Removed unused template builder

--- Solutions/buildtemplates.ps1
+++
@@ -1,8 +0,0 @@
-# Change directory to the Corvus.Json directory
-Set-Location -Path 'Corvus.Json.ExtendedTypes\Corvus.Json'
-
-# Run the BuildTemplates.ps1 script
-& '.\BuildTemplates.ps1'
-
-# Change back to the original directory
-Set-Location -Path '..\..'
