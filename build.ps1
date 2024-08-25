ac6f424feaac39e441376d7dfecd95ec492717ab
James Dawson <james.dawson@endjin.com>
2024-08-25 23:30:22 +0100

    Remove build settings that are now redundant with latest scripted build version

--- build.ps1
+++ build.ps1
@@ -143,22 +143,6 @@ $NuSpecFilesToPackage = @(
     # "Solutions/MySolution/MyProject/MyProject.nuspec"
 )
 
-#
-# Specify files to exclude from code coverage
-# This option is for excluding generated code
-# - Use file path or directory path with globbing (e.g dir1/*.cs)
-# - Use single or multiple paths (separated by comma) (e.g. **/dir1/class1.cs,**/dir2/*.cs,**/dir3/**/*.cs)
-#
-# NOTE: These exclusions suppress errors from the test report tool caused by a mismatch of relative paths
-#       between the code generation and the test report tooling.
-# $ExcludeFilesFromCodeCoverage = ""
-
-#
-# Update to the latest report generator versions
-#
-$ReportGeneratorToolVersion = "5.1.10"
-$CovenantVersion = "0.19.0"
-
 $CreateGitHubRelease = $true
 $PublishNuGetPackagesAsGitHubReleaseArtefacts = $true
 
