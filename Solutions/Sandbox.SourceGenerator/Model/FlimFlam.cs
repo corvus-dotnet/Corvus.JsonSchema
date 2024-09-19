f79731c17b54f4ede11b805f407b33def449a86b
Matthew Adams <matthew.adams@endjin.com>
2024-09-19 17:33:53 +0100

    Initial working commit for local package reference.

---
+++ Solutions/Sandbox.SourceGenerator/Model/FlimFlam.cs
@@ -0,0 +1,8 @@
+﻿
+using Corvus.Json;
+
+namespace SourceGenTest2.Model;
+[JsonSchemaTypeGenerator("../test.json")]
+public readonly partial struct FlimFlam
+{
+}
---
+++ Solutions/Sandbox.SourceGenerator/WorkingWithGeneratedCode.cs
@@ -0,0 +1,14 @@
+﻿using Corvus.Json;
+using SourceGenTest2.Model;
+
+namespace Sandbox.SourceGenerator;
+public static class WorkingWithGeneratedCode
+{
+    public static void Process()
+    {
+        FlimFlam flimFlam = JsonAny.ParseValue("[1,2,3]"u8);
+        Console.WriteLine(flimFlam);
+        JsonArray array = flimFlam.As<JsonArray>();
+        Console.WriteLine(array);
+    }
+}
