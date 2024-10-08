4f84622176b99d95c5690dc7587aba6e188fa8de
Matthew Adams <matthew.adams@endjin.com>
2024-10-08 21:05:35 +0100

    Updated to .NET 9.0 RC2 bits.

--- Solutions/Corvus.Json.Benchmarking/Program.cs
+++ Solutions/Corvus.Json.Benchmarking/Program.cs
@@ -7,8 +7,6 @@ using BenchmarkDotNet.Engines;
 using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Toolchains.CsProj;
-using BenchmarkDotNet.Toolchains.DotNetCli;
 using Perfolizer.Mathematics.OutlierDetection;
 
 internal class Program
@@ -24,12 +22,8 @@ internal class Program
                 .WithStrategy(RunStrategy.Throughput));
 
         config.AddJob(
-            Job.Default.WithToolchain(
-                CsProjCoreToolchain.From(
-                    new NetCoreAppSettings(
-                        targetFrameworkMoniker: "net9.0",
-                        runtimeFrameworkVersion: "9.0.0-rc.2.24461.16",
-                        name: ".NET 9.0 RC2 Nightly")))
+            Job.Default
+                .WithRuntime(CoreRuntime.Core90)
                 .WithOutlierMode(OutlierMode.RemoveAll)
                 .WithStrategy(RunStrategy.Throughput));
 
