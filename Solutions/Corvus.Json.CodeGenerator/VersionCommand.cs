454e7901dcbafdb0d34ea1cd7c4ed7e0c791be36
Matthew Adams <matthew.adams@endjin.com>
2024-11-07 14:51:54 +0000

    Added version command to address #475

---
+++ Solutions/Corvus.Json.CodeGenerator/VersionCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Corvus.Json.CodeGenerator;
+
+/// <summary>
+/// Spectre.Console.Cli command to display the product version.
+/// </summary>
+internal class VersionCommand : Command
+{
+    public override int Execute(CommandContext context)
+    {
+        AnsiConsole.MarkupLineInterpolated($"[green]Version:[/] {(Assembly.GetEntryAssembly()?.GetName()?.Version?.ToString()) ?? "Not available."}");
+
+        return 1;
+    }
+}
