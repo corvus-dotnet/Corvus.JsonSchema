4cb78d619860c6bd733371accd4a33e1672f9365
Matthew Adams <matthew.adams@endjin.com>
2024-11-29 10:05:09 +0000

    Added YAML preprocessor Added YAML support to the code generator `--yaml` switch enables YAML support. Additional files also support YAML if `--yaml` is enabled.

---
+++ Solutions/Corvus.Json.CodeGeneration/Corvus.Json.CodeGeneration/DocumentResolvers/IDocumentStreamPreProcessor.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="IDocumentStreamPreProcessor.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Corvus.Json.CodeGeneration.DocumentResolvers;
+
+/// <summary>
+/// Pre-process a document stream.
+/// </summary>
+public interface IDocumentStreamPreProcessor
+{
+    /// <summary>
+    /// Pre-processes the input stream to produce the output stream.
+    /// </summary>
+    /// <param name="input">The input document stream.</param>
+    /// <returns>The output stream.</returns>
+    Stream Process(Stream input);
+}
